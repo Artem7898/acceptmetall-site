38cce87188e41ff9ca34c59d7405da914b110c4c
Artem <alimpievne@gmail.com>
2026-08-25 17:42:13 +0300

    fix: correctly apply Astro.base to favicon and assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,28 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://akceptmetall.ru',
+
+  // Пока сайт работает в поддиректории.
+  // После покупки и подключения akceptmetall.ru
+  // этот параметр можно будет удалить.
   base: '/acceptmetall-site/',
+
   output: 'static',
+
+  // Все страницы будут использовать trailing slash:
+  // /en/
+  // /privacy/
+  // /en/privacy/
+  trailingSlash: 'always',
+
   i18n: {
     defaultLocale: 'ru',
     locales: ['ru', 'en'],
+
     routing: {
-      prefixDefaultLocale: false
-    }
-  }
+      // Русский — основной язык без /ru/
+      // Английский — /en/
+      prefixDefaultLocale: false,
+    },
+  },
 });
